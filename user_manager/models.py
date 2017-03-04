1590861b63dd5c58733177274931c3862bce5955
nparadise <s_nakwon@naver.com>
2017-03-04 23:49:13 +0900

    폼 blank true

--- user_manager/models.py
+++ user_manager/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class UserDetail(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	school = models.CharField(max_length=30)
-	major = models.CharField(max_length=30)
-	phone = models.CharField(max_length=20)
-	birthday = models.DateField()
+	school = models.CharField(max_length=30, blank=True)
+	major = models.CharField(max_length=30, blank=True)
+	phone = models.CharField(max_length=20, blank=True)
+	birthday = models.DateField(blank=True)
 
 	def __str__(self):
 		return str(self.user)
